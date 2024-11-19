a321cad2178997b4b68691a086b65512fb0c302e
likhith-gowda-2004 <149521446+likhith-gowda-2004@users.noreply.github.com>
2024-11-19 22:50:14 +0530

    Modified Pubsub

--- subscriber1.py
+++ subscriber1.py
@@ -11,16 +11,15 @@ def create_consumer():
         'sub_1',
         bootstrap_servers='localhost:9092',
         value_deserializer=lambda x: json.loads(x.decode('utf-8')),
-        auto_offset_reset='earliest',  # This can be modified as needed
-        enable_auto_commit=True,
-        group_id=None  # No group ID ensures each consumer gets its own copy of messages
+        #auto_offset_reset='earliest',  # This can be modified as needed
+        #enable_auto_commit=True,
+        #group_id=None  # No group ID ensures each consumer gets its own copy of messages
     )
 
 # Generator function for sending emojis to clients
 def send_emoji():
     consumer = create_consumer()  # Create a new consumer for each client
     for msg in consumer:
-        print(msg.value)
         yield f"data: {json.dumps(msg.value)}\n\n"  # Formatting as Server-Sent Events (SSE)
 
 @app.route('/sub1')
